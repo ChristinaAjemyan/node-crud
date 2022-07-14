eba8e7a13a4ab5db4e3fca9b70e7e4451a9fdf86
alienforhumans <gurgen.varzhapetyan@mail.ru>
2022-07-14 11:35:12 +0400

    Added model validations for User model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,28 @@
 const {Schema, model} = require("mongoose");
 
+// Validators
+const validateEmail = require("../validators/email-validator");
+
 const User = model(
     "User",
     new Schema({
-        username: String,
-        email: String,
-        password: String,
-        // Pno to many relation with roles collection
+        username: {
+            type: String,
+            required: 'Username is required'
+        },
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            required: 'Email address is required',
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        password: {
+            type: String,
+            required: 'Password is required'
+        },
         roles: [
             {
                 type: Schema.Types.ObjectId,
